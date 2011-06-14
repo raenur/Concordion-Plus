7949ba671871c3114d146e62d7a23b5f59a8a726
agileinsider@gmail.com <agileinsider@gmail.com@f5fa0896-1729-cf1b-2d0a-608c1b9877bc>
2011-06-14 08:11:27 +0000

    Removing copyright from IDEA config...
    git-svn-id: http://concordion-plus.googlecode.com/svn/trunk@37 f5fa0896-1729-cf1b-2d0a-608c1b9877bc

--- src/main/java/org/agileinsider/concordion/command/ScenarioCommand.java
+++ src/main/java/org/agileinsider/concordion/command/ScenarioCommand.java
@@ -1,28 +1,11 @@
 package org.agileinsider.concordion.command;
 
-/*
- * Copyright 2011 Mark Barnes (mark@agileinsider.org)
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
-
-import org.agileinsider.concordion.ConcordionPlusExtension;
-import org.agileinsider.concordion.ScenarioExtension;
-import org.agileinsider.concordion.event.*;
-
 import ognl.DefaultMemberAccess;
 import ognl.Ognl;
 import ognl.OgnlContext;
+import org.agileinsider.concordion.ConcordionPlusExtension;
+import org.agileinsider.concordion.ScenarioExtension;
+import org.agileinsider.concordion.event.*;
 import org.concordion.api.*;
 import org.concordion.internal.OgnlEvaluator;
 import org.concordion.internal.util.Announcer;
--- src/test/java/org/agileinsider/concordion/ConcordionPlusAcceptanceTest.java
+++ src/test/java/org/agileinsider/concordion/ConcordionPlusAcceptanceTest.java
@@ -1,26 +1,9 @@
 package org.agileinsider.concordion;
 
-/*
- * Copyright 2011 Mark Barnes (mark@agileinsider.org)
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
-
 import org.agileinsider.concordion.testutil.ConcordionTestHarness;
 import org.agileinsider.concordion.testutil.JUnitTestHarness;
 import org.agileinsider.concordion.testutil.JUnitTestHarnessRunNotifier;
 import org.agileinsider.concordion.testutil.ProcessingResult;
-
 import org.concordion.api.ExpectedToFail;
 import org.junit.After;
 import org.junit.runner.RunWith;
